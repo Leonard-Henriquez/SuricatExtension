423350b2c1c4711327e155c66c7991d3813be40e
Leonard-Henriquez <leonard.henriquez@edhec.com>
2018-11-05 12:17:46 +0100

    scrapping POC

--- src/js/content.js
+++ src/js/content.js
@@ -15,14 +15,61 @@ import msg from './modules/msg';
 
 console.log('CONTENT SCRIPT WORKS!'); // eslint-disable-line no-console
 
+const scrapContent = (url) => {
+  const basisSelector = '.job-offer-container';
+  const data = { url };
+  let title;
+  let content;
+
+  const cssSelectors = {
+    title: '.job-offer-top--title',
+    company_name: '.job-offer-top--company-name',
+    contract_type: '.job-offer-top--infos span:nth-child(1)',
+    location: '.job-offer-top--infos span:nth-child(2)',
+    job_description: '.job-offer-container--text',
+  };
+
+  $.each(cssSelectors, (k, v) => {
+    data[k] = $(`${basisSelector} ${v}`).html();
+  });
+
+  $(`${basisSelector} .job-offer-details > ul > li`).each((i, e) => {
+    title = $(e).children('h6').text();
+    content = $(e).children('p').text();
+    if (title === 'Start date') {
+      data.start_date = content;
+    } else if (title === 'Related jobs') {
+      data.job_title = content;
+    } else if (title === 'Business type') {
+      data.company_structure = content;
+    }
+  });
+
+  data.image = $('meta[property="og:image"]').attr('content');
+
+  const regexes = {
+    salary: /\d[ \d]{1,5}(?:,\d{2})?[€$£]|[$£][ ,\d]{2,6}(?:.\d{2})?/,
+    email: /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/
+  };
+
+  $.each(regexes, (k, v) => {
+    content = v.exec(data.job_description);
+    if (content) {
+      data[k] = content[0];
+    }
+  });
+
+  console.log(data);
+  return data;
+};
+
+
 const contentHandlers = {
   getContent: (url) => {
     console.log(`Scrapping "${url}"...`);
-    const data = { url };
+    const data = scrapContent(url);
     message.bg('createOpportunity', data);
   }
 };
 
 const message = msg.init('ct', contentHandlers);
-
-console.log('jQuery version:', $().jquery); // eslint-disable-line no-console
